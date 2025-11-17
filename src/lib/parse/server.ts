bb1a10ade629678a4b5848650b42db4a1c9d4796
Khaulan McLean <Khaulan1984@gmail.com>
2025-11-17 22:14:51 +0100

    added profile edit page

--- src/lib/parse/server.ts
+++ src/lib/parse/server.ts
@@ -1,18 +1,18 @@
 import * as dotenv from "dotenv";
-dotenv.config({ path: '.env.local' });
+dotenv.config({ path: ".env.local" });
 import Parse from "parse/node.js";
-
-console.log('ENV:', {
-    APP_ID: process.env.VITE_PARSE_APP_ID,
-    JS_KEY: process.env.VITE_PARSE_JS_KEY,
-    SERVER_URL: process.env.VITE_PARSE_SERVER_URL,
-    MASTER_KEY: process.env.PARSE_MASTER_KEY
+//Should we log master keys, even in development here?
+console.log("ENV:", {
+  APP_ID: process.env.VITE_PARSE_APP_ID,
+  JS_KEY: process.env.VITE_PARSE_JS_KEY,
+  SERVER_URL: process.env.VITE_PARSE_SERVER_URL,
+  MASTER_KEY: process.env.PARSE_MASTER_KEY,
 });
 
 Parse.initialize(
-    process.env.VITE_PARSE_APP_ID!,
-    process.env.VITE_PARSE_JS_KEY!,
-    process.env.PARSE_MASTER_KEY!
+  process.env.VITE_PARSE_APP_ID!,
+  process.env.VITE_PARSE_JS_KEY!,
+  process.env.PARSE_MASTER_KEY!
 );
 Parse.serverURL = process.env.VITE_PARSE_SERVER_URL!;
 
